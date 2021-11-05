83df6a33b381dee988bfcf9f79b4aefe07bbad83
Alan <alanhp2@hotmail.com>
2021-11-04 18:18:53 -0700

    fixed jsx

--- src/components/Submitted.js
+++ src/components/Submitted.js
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom'
 
 export default function Submitted() {
     return (
-        <div>       
-            <div className="submittedContainer">               
+        <div className="submittedContainer">
+            <h1>Submitted!</h1>
+            <br/>
+            <h3>Please sign out or close window</h3>
+            {/* <div className="submittedContainer">               
                 <label>Submitted!</label>                 
             </div> 
             <div className="containerBelowSubmittedContainer">
                 <label>Please sign out or close window.</label>
-            </div>     
+            </div>      */}
         </div>
     )
 }
