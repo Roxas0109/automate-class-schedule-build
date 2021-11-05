b641e23d12f2285eea187039f4dddf8b503fe0b1
Alan <alanhp2@hotmail.com>
2021-11-04 19:19:18 -0700

    removed link in import

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,6 +1,5 @@
 import './Import.css'
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function Import() {
     return (
@@ -9,9 +8,7 @@ export default function Import() {
                 <div className='content-view'>
                     <h1>Import Degree Progress Report (DPR)</h1>
                     <div className="continueBtn">
-                    <Link to={'submitted'}>
                         <button class="redBtn">Import File</button>
-                    </Link>
                     </div>
                 </div>
             </div>
