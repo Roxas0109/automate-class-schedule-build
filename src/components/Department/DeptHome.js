ce96ab9caf78a30b0f78f37e8fd96177211e4aaf
Alan Hernandez <alanhp2@hotmail.com>
2022-03-02 22:49:40 -0800

    working pagination need styling

--- src/components/Department/DeptHome.js
+++ src/components/Department/DeptHome.js
@@ -1,14 +1,46 @@
 import './DeptHome.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ReactPaginate from 'react-paginate'
 
 export default function DeptHome() {
+    const JSONTest = [{ class: "COMP1", allocation: "357" }, { class: "COMP2", allocation: "357" },
+    { class: "COMP3", allocation: "357" }, { class: "COMP4", allocation: "357" }, { class: "COMP5", allocation: "357" },
+    { class: "COMP6", allocation: "357" }, { class: "COMP7", allocation: "357" }]
 
-    const JSONTest = [{ class: "COMP222", allocation: "357" },{ class: "COMP222", allocation: "357" },
-    { class: "COMP222", allocation: "357" },{ class: "COMP222", allocation: "357" }, { class: "COMP222", allocation: "357" },
-    { class: "COMP222", allocation: "357" }]
+    const [courses, setCourses] = useState([])
 
-    function displayCourses() {
+    useEffect(() => {
+        setCourses(JSONTest)
+    }, [])
+
+    const [pageNumber, setPageNumber] = useState(0)
+
+    const numPerPage = 2
+    const pagesVisited = pageNumber * numPerPage
+
+    const pageCount = Math.ceil(courses.length / numPerPage)
+
+    const displayCourses = courses
+        .slice(pagesVisited, pagesVisited + numPerPage)
+        .map((item) => {
+            return (
+                <tr>
+                    <td>
+                        {item.class}
+                    </td>
+                    <td>
+                        {item.allocation}
+                    </td>
+                </tr>
+            )
+        })
+
+    const changePage = ({ selected }) => {
+        setPageNumber(selected)
+    }
+    
+    function createTable() {
         return (
             <>
                 <table>
@@ -23,23 +55,11 @@ export default function DeptHome() {
                         </tr>
                     </thead>
                     <tbody>
-                        {JSONTest.map(item => {
-                            return (
-                                <tr>
-                                    <td>
-                                        {item.class}
-                                    </td>
-                                    <td>
-                                        {item.allocation}
-                                    </td>
-                                </tr>
-                            )
-                        })}
+                        {displayCourses}
                     </tbody>
                 </table>
             </>)
     }
-
 
 
     return (
@@ -48,7 +68,18 @@ export default function DeptHome() {
         <div className="tableContainer">
             <div className="wrapper">
                 <div className="allocationsTable"></div>
-                {displayCourses()}
+                {createTable()}
+                <ReactPaginate
+                    previousLabel={'Previous'}
+                    nextLabel={'Next'}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={'paginationBtns'}
+                    previousClassName={'prevBtn'}
+                    nextLinkClassName={'nextBtn'}
+                    disabledClassName={'pagDisabled'}
+                    activeClassName={'pagActive'}
+                />
                 <button type="submit" className="csn-btn" onclick="myFunction()" >
                     <FontAwesomeIcon icon="redo" />
                     Generate Allocation</button>
